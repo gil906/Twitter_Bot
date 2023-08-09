87ab97ad80065024f75246469d28fceca4990955
Gilbert De Leon <gdleonq@gmail.com>
2023-08-08 23:22:39 -0700

    Update x_bot.py

--- x_bot.py
+++ x_bot.py
@@ -1,6 +1,6 @@
 import tweepy
 
-# Twitter API credentials 
+# Twitter API credentials
 consumer_key = "YOUR_CONSUMER_KEY"
 consumer_secret = "YOUR_CONSUMER_SECRET"
 access_token = "YOUR_ACCESS_TOKEN"
@@ -11,56 +11,63 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+# Define reply triggers and messages
+reply_triggers = {
+    "hello": "Hello @{mention.user.screen_name}! How can I assist you today?",
+    "hi": "Hello @{mention.user.screen_name}! How can I assist you today?",
+    "help": "Sure, I'm here to help! Please let me know what you need assistance with.",
+    "support": "Sure, I'm here to help! Please let me know what you need assistance with.",
+    "thanks": "You're welcome, @{mention.user.screen_name}! If you have any more questions, feel free to ask.",
+    "thank you": "You're welcome, @{mention.user.screen_name}! If you have any more questions, feel free to ask."
+}
+
 # Define the function to handle mentions
 def reply_to_mentions():
     mentions = api.mentions_timeline()
     
-    # Batch processing for mentions
-    mention_actions = []
+    # Prepare reply actions
+    reply_actions = []
     
     for mention in mentions:
-        # Get the mention text in lowercase
         mention_text = mention.text.lower()
-        
-        # Default reply message
         reply_text = f"Thank you for reaching out, @{mention.user.screen_name}!"
         
-        # Customize the replies based on different triggers
-        if "hello" in mention_text or "hi" in mention_text:
-            reply_text = f"Hello @{mention.user.screen_name}! How can I assist you today?"
-        elif "help" in mention_text or "support" in mention_text:
-            reply_text = f"Sure, I'm here to help! Please let me know what you need assistance with."
-        elif "thanks" in mention_text or "thank you" in mention_text:
-            reply_text = f"You're welcome, @{mention.user.screen_name}! If you have any more questions, feel free to ask."
+        for trigger, response in reply_triggers.items():
+            if trigger in mention_text:
+                reply_text = response
+                break
         
-        # Create actions for the mention
-        mention_actions.append({
+        reply_actions.append({
             'mention_id': mention.id,
             'reply_text': reply_text,
             'user_screen_name': mention.user.screen_name
         })
     
-    # Perform actions in a single API call
-    for action in mention_actions:
-        try:
-            # Reply to the mention
-            api.update_status(
-                status=action['reply_text'],
-                in_reply_to_status_id=action['mention_id']
-            )
-            print(f"Replied to mention from @{action['user_screen_name']}")
+    # Prepare bulk reply data
+    reply_data = {
+        action['mention_id']: action['reply_text'] for action in reply_actions
+    }
+    
+    # Perform bulk actions
+    try:
+        # Bulk reply to mentions
+        api.update_statuses(status=reply_data)
+        print(f"Replied to mentions")
 
-            # Retweet and like the mention
-            api.retweet(action['mention_id'])
-            api.create_favorite(action['mention_id'])
+        # Get mention IDs for other actions
+        mention_ids = [action['mention_id'] for action in reply_actions]
 
-            # Follow the user who mentioned the bot
-            api.create_friendship(action['user_screen_name'])
-            print(f"Followed user: @{action['user_screen_name']}")
-            
-        except tweepy.TweepError as e:
-            print(f"Error occurred: {e}")
-            continue
+        # Bulk retweet and like
+        api.retweet(mention_ids)
+        api.create_favorite(mention_ids)
+
+        # Bulk follow
+        for user_screen_name in set(action['user_screen_name'] for action in reply_actions):
+            api.create_friendship(user_screen_name)
+            print(f"Followed user: @{user_screen_name}")
+
+    except tweepy.TweepError as e:
+        print(f"Error occurred: {e}")
 
 # Run the bot
 reply_to_mentions()
